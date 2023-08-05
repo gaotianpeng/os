495de8a8465c225ede315c33b9f2d17c86c86ef7
gaotianpeng <gtpsydm@gmail.com>
2023-08-05 19:49:45 +0800

    突破512字节限制

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ all: ${BUILD}/boot/boot.o
 	$(shell rm -rf $(HD_IMG_NAME))
 	bximage -q -hd=16 -func=create -sectsize=512 -imgmode=flat $(HD_IMG_NAME)
 	dd if=${BUILD}/boot/boot.o of=hd.img bs=512 seek=0 count=1 conv=notrunc
+	dd if=${BUILD}/boot/boot.o of=hd.img bs=512 seek=1 count=1 conv=notrunc
 
 ${BUILD}/boot/%.o: oskernel/boot/%.asm
 	$(shell mkdir -p ${BUILD}/boot)
--- oskernel/boot/boot.asm
+++ oskernel/boot/boot.asm
@@ -1,56 +1,67 @@
-[ORG 0X7C00]
+; 0柱面0磁道1扇區
+[ORG  0x7c00]
+
+[SECTION .data]
+BOOT_MAIN_ADDR equ 0x500
 
 [SECTION .text]
 [BITS 16]
 global _start
 _start:
-    ; 设置屏幕模式为文本模式，并清除屏幕
+    ; 设置屏幕模式为文本模式，清除屏幕
     mov ax, 3
     int 0x10
 
-    xor ax, ax
-    mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov si, ax
+    ; 将bootsect读入0x7e00
+    ; 读盘
+    mov     ch, 0   ; 0 柱面
+    mov     dh, 0   ; 0 磁头
+    mov     cl, 2   ; 2 扇区
+    mov     bx, BOOT_MAIN_ADDR  ; 数据往哪读
 
-    ; 设置前景色和背景色
-    xor ax, ax
-    mov ah, 0x0B   ; AH = 0x0B (Function to set background and foreground colors)
-    mov bh, 0x00   ; BH = 0x00 (Background color, in this case, black)
-    mov bl, 0x01   ; BL = 0x0E (Foreground color, in this case, blue)
-    int 0x10
-    ; 调置字体
-    xor ax, ax
-    mov ah, 0x11   ; AH = 0x11 (Function to set the font)
-    mov al, 0x14   ; AL = 0x14 (Font number, in this case, 8x14 font)
-    int 0x10       ; Call BIOS video interrupt to set the font
+    mov     ah, 0x02    ; 读盘操作
+    mov     al, 1       ; 连续读几个扇区
+    mov     dl, 0       ; 驱动器编号
 
-    mov si, msg     ; 传入字符串
-    call print
+    int     0x13
+
+    ; 跳过去
+    mov     si, jmp_to_setup
+    call    print
+
+    xchg bx, bx
+
+    jmp     BOOT_MAIN_ADDR
+
+read_floppy_error:
+    mov     si, read_floppy_error_msg
+    call    print
 
     jmp $
 
+; 如何调用
+; mov     si, msg   ; 1 传入字符串
+; call    print     ; 2 调用
 print:
-    ; ah 代表欲调用的功能
-    mov ah, 0x0e    ; e显示字符，光标随字符移动(al字符，bl前景色)
-    mov bh, 0x00
+    mov ah, 0x0e
+    mov bh, 0
     mov bl, 0x01
-.loop
+.loop:
     mov al, [si]
     cmp al, 0
-    jz .done        ; 遇到0打印结束
-    int 0x10        ; 打印字符并移动光标
+    jz .done
+    int 0x10
 
     inc si
     jmp .loop
-.done
+.done:
     ret
 
-msg:
-    db "hello world!", 10, 13, 0
+read_floppy_error_msg:
+    db "read floppy error!", 10, 13, 0
 
-times 510 - ($ - $$) db 0x00
-db 0x55,0xaa
+jmp_to_setup:
+    db "jump to setup...", 10, 13, 0
+
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
