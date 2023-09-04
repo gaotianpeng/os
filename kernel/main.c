2453e0aa192140293595886cb6a49baf53decf40
gaotianpeng <gtpsydm@gmail.com>
2023-09-04 15:27:41 +0800

    print int

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,16 @@
 #include "../lib/kernel/print.h"
 
 void main(void) {
-    put_str("I am kernel\n");
-    put_str("2023-09-03\n");
-    while(1);
+   put_str("I am kernel\n");
+   put_int(0);
+   put_char('\n');
+   put_int(9);
+   put_char('\n');
+   put_int(0x00021a3f);
+   put_char('\n');
+   put_int(0x12345678);
+   put_char('\n');
+   put_int(0x00000000);
+   
+   while (1);
 }
