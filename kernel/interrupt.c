1704a68cce5b53f2d5259dc4fe51507f5098093b
gaotianpeng <gtpsydm@gmail.com>
2023-09-08 10:10:03 +0800

    keyboard

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -9,7 +9,7 @@
 #define PIC_S_CTRL 0xa0	        // 从片的控制端口是0xa0
 #define PIC_S_DATA 0xa1	        // 从片的数据端口是0xa1
 
-#define IDT_DESC_CNT 0x21	    // 目前总共支持的中断数
+#define IDT_DESC_CNT 0x30	    // 目前总共支持的中断数
 
 #define EFLAGS_IF   0x00000200       // eflags寄存器中的if位为1
 #define GET_EFLAGS(EFLAG_VAR) asm volatile("pushfl; popl %0" : "=g" (EFLAG_VAR))
@@ -46,8 +46,8 @@ static void pic_init(void) {
     outb (PIC_S_DATA, 0x02);	// ICW3: 设置从片连接到主片的IR2引脚
     outb (PIC_S_DATA, 0x01);	// ICW4: 8086模式, 正常EOI
 
-    // 打开主片上IR0，目前只接受时钟产生的中断
-    outb (PIC_M_DATA, 0xfe);    // 不屏蔽IR0的时钟中断。其他位都是1，表示都屏蔽
+    // 只打开键盘中断
+    outb (PIC_M_DATA, 0xfd);
     outb (PIC_S_DATA, 0xff);    // 从片上的所有外设都屏蔽
 
     put_str("   pic_init done\n");
