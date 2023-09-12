82a54c06928fafcf19ea9ca0b1f5a89b991e768f
gaotianpeng <gtpsydm@gmail.com>
2023-09-12 20:59:47 +0800

    printf

--- kernel/main.c
+++ kernel/main.c
@@ -17,23 +17,23 @@ int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   process_execute(u_prog_a, "user_prog_a");
-   process_execute(u_prog_b, "user_prog_b");
+   process_execute(u_prog_a, "u_prog_a");
+   process_execute(u_prog_b, "u_prog_b");
 
-   intr_enable();
-   console_put_str(" main_pid:0x");
+   console_put_str(" I am main, my pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
-   thread_start("k_thread_a", 31, k_thread_a, "argA ");
-   thread_start("k_thread_b", 31, k_thread_b, "argB ");
-   while(1);
+   intr_enable();
+   thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
+   thread_start("k_thread_b", 31, k_thread_b, "I am thread_b ");
 
+   while(1);
    return 0;
 }
 
 void k_thread_a(void* arg) {     
    char* para = arg;
-   console_put_str(" thread_a_pid:0x");
+   console_put_str(" I am thread_a, my pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
    while(1);
@@ -41,21 +41,20 @@ void k_thread_a(void* arg) {
 
 void k_thread_b(void* arg) {     
    char* para = arg;
-   console_put_str(" thread_b_pid:0x");
+   console_put_str(" I am thread_b, my pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
-
    while(1);
 }
 
 void u_prog_a(void) {
-   printf(" prog_a_pid:0x%x\n", getpid());
-
+   char* name = "prog_a";
+   printf(" I am %s, my pid:%d%c", name, getpid(), '\n');
    while(1);
 }
 
 void u_prog_b(void) {
-   printf(" prog_b_pid:0x%x\n", getpid());
-
+   char* name = "prog_b";
+   printf(" I am %s, my pid:%d%c", name, getpid(), '\n');
    while(1);
 }
