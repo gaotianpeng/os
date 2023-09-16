49c801acb5fbbb267690c071a22d220f9dd3cb30
gaotianpeng <gtpsydm@gmail.com>
2023-09-16 20:14:04 +0800

    fs:opendir/closedir

--- fs/fs.c
+++ fs/fs.c
@@ -710,3 +710,41 @@ void filesys_init() {
         file_table[fd_idx++].fd_inode = NULL;
     }
 }
+
+// 目录打开成功后返回目录指针，失败返回NULL
+struct dir* sys_opendir(const char* name) {
+    ASSERT(strlen(name) < MAX_PATH_LEN);
+
+    // 如果是根目录'/', 直接返回&root_dir
+    if (name[0] == '/' && (name[1] == 0 || name[0] == './')) {
+        return &root_dir;
+    }
+
+    // 先检查待打开的目录是否存在
+    struct path_search_record searched_record;
+    memset(&searched_record, 0, sizeof(struct path_search_record));
+    int inode_no = search_file(name, &searched_record);
+    struct dir* ret = NULL;
+    if (inode_no == -1) {	 // 如果找不到目录,提示不存在的路径 
+        printk("In %s, sub path %s not exist\n", name, searched_record.searched_path); 
+    } else {
+        if (searched_record.file_type == FT_REGULAR) {
+            printk("%s is regular file!\n", name);
+        } else if (searched_record.file_type == FT_DIRECTORY) {
+            ret = dir_open(cur_part, inode_no);
+        }
+    }
+    dir_close(searched_record.parent_dir);
+    return ret;
+}
+
+// 成功关闭目录dir返回0, 失败则返回-1
+int32_t sys_closedir(struct dir* dir) {
+    int32_t ret = -1;
+    if (dir != NULL) {
+        dir_close(dir);
+        ret = 0;
+    }
+
+    return ret;
+}
